a22a7e142f550c7a5d30a0337df24650ae63170d
unknown <dane.hoffman90@gmail.com>
2020-05-13 11:35:50 -0600

    Initial commit w/ a 'Game', 'Player', 'Computer', and 'Human' class

---
+++ R-P-S-L-S-GameProject/Computer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R_P_S_L_S_GameProject
+{
+    class Computer
+    {
+
+
+
+    }
+}
---
+++ R-P-S-L-S-GameProject/Player.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace R_P_S_L_S_GameProject
+{
+    class Player
+    {
+
+
+
+    }
+}
